50d57326f80561b11b12165122185eb707a70bdd
elDone <diego-o.o-@hotmail.com>
2025-06-03 20:51:48 -0500

    feat: a;adiendo logging con logback

--- microservicios/common-exceptions/src/main/java/com/idone/microservices/common/exceptions/GlobalExceptionHandler.java
+++ microservicios/common-exceptions/src/main/java/com/idone/microservices/common/exceptions/GlobalExceptionHandler.java
@@ -10,31 +10,34 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException exception) {
-
+    @ExceptionHandler(MethodArgumentNotValidException.class) 
+    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException exception) { 
+        
         var errors = new HashMap<String, String>();
         exception.getBindingResult().getFieldErrors().forEach(error -> {
             var fieldName = ((FieldError) error).getField();
             var errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors));
+        log.warn("Validation errors: {}", exception.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors)); 
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
-        var errors = new HashMap<String, String>();
-        var fieldName = "message";
-        var errorMessage = "Se ha producido un error. Por favor, contacte al administrador o intente mas tarde.";
-        errors.put(fieldName, errorMessage);
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(errors));
-    };
+    @ExceptionHandler(Exception.class) 
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) { 
+        var errors = new HashMap<String, String>(); 
+        var fieldName = "message"; 
+        var errorMessage = "An error has occurred. Please contact the administrator or try again later.";
+        errors.put(fieldName, errorMessage); 
+        log.error("Error: {}",exception.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(errors)); 
+    }
 
 }
--- microservicios/customer-microservices/src/main/java/com/idone/microservices/customer_microservices/exceptions/CustomerExceptionHandler.java
+++ microservicios/customer-microservices/src/main/java/com/idone/microservices/customer_microservices/exceptions/CustomerExceptionHandler.java
@@ -11,8 +11,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.idone.microservices.common.exceptions.ErrorResponse;
 import com.idone.microservices.common.exceptions.GlobalExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestControllerAdvice(basePackages = "com.idone.microservices.customer_microservices")
 @Primary
+@Slf4j
 public class CustomerExceptionHandler extends GlobalExceptionHandler{
 
     @ExceptionHandler(CustomerNotFoundException.class)
@@ -22,6 +25,7 @@ public class CustomerExceptionHandler extends GlobalExceptionHandler{
         var errors = new HashMap<String, String>();
         var fieldName = "customer";
         errors.put(fieldName, exception.getMessage());
+        log.warn("Customer not found: {}", exception.toString());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors)); 
 
